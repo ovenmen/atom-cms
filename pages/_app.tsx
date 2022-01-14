400fde92208998b55ca2b00ca2dc91d029af5f52
Антон Детев <detanton@gmail.com>
2022-01-14 20:26:33 +0400

    feat: Изменен шрифт на Open Sans

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import { AppProps } from 'next/app';
+
+import 'semantic-ui-css/semantic.min.css';
+
+const AtomCMSApp: FC<AppProps> = ({
+    Component,
+    pageProps
+}) => (
+    <>
+        <Component {...pageProps} />
+
+        <style global jsx>{`
+            * {
+                font-family: 'Open Sans', serif !important;
+            }
+        `}</style>
+    </>
+);
+
+export default AtomCMSApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
-const Home = () => (
-    <section>Main Page</section>
+import { FC } from 'react';
+import { Header } from 'semantic-ui-react';
+
+const Home: FC = () => (
+    <Header as='h1'>Главная страница</Header>
 );
 
 export default Home;

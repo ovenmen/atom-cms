e7cf14856043b3317f9cde2a6e67fa31b0a7987c
Антон Детев <detanton@gmail.com>
2022-02-14 23:41:20 +0400

    feat: Добавлено респонсивное меню

--- components/app-navigation.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AppNavigation = () => (
-    <p></p>
-);
-
-export default AppNavigation;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import { FC } from 'react';
 import { Container, Grid } from '@mui/material';
 
-import SideMenu from '../components/side-menu';
+import ResponsiveAppBar from '../components/responsive-app-bar';
 
 const Home: FC = () => (
     <Container maxWidth="lg">
         <Grid container spacing={2}>
-            <Grid item xs={2}>
-                <SideMenu />
+            <Grid item xs={12}>
+                <ResponsiveAppBar />
             </Grid>
-            <Grid item xs={10}>
+            <Grid item xs={12}>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta distinctio quam molestiae quos tempora minus odit ad veritatis deleniti cum itaque quibusdam tempore cupiditate harum modi atque, quaerat nobis hic.
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ab autem placeat, a expedita vero similique maxime eaque temporibus porro, labore modi tempora laborum corrupti repudiandae sed est saepe earum?
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, voluptatibus! Rem, temporibus obcaecati commodi dolores eius, repellendus, dolorum mollitia et alias ut consequuntur dignissimos! Rerum eveniet repellendus vero error provident?

af0da12ae9b38849b9462c5e280f0eb4a2dda2ae
Антон Детев <detanton@gmail.com>
2022-02-23 22:12:20 +0400

    feat: Переход на Semantic-UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { AppProps } from 'next/app';
+import 'semantic-ui-css/semantic.min.css';
 
 const AtomCMSApp: FC<AppProps> = ({
     Component,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,11 @@
 import { FC } from 'react';
-import { Container, Grid } from '@mui/material';
-
-import ResponsiveAppBar from '../components/responsive-app-bar';
-import Post from '../components/post';
+import { Button, Container, Grid } from 'semantic-ui-react';
 
 const Home: FC = () => (
-    <Container maxWidth="lg">
-        <Grid container spacing={2}>
-            <Grid item xs={12}>
-                <ResponsiveAppBar />
-            </Grid>
-            <Grid item xs={12}>
-                <Grid container spacing={2}>
-                    {[0,1,2,3,4,5,6,7,8,9].map((post, index) => (
-                        <Grid item xs={12} sm={6} lg={3} md={4} key={index}>
-                            <Post key={index} />
-                        </Grid>
-                    ))}
-                </Grid>
-            </Grid>
-        </Grid>
+    <Container>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium aliquam quibusdam tempora eum aliquid accusamus obcaecati! Repellat mollitia modi expedita laborum, at, illo cumque beatae molestiae inventore harum rerum dignissimos!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium aliquam quibusdam tempora eum aliquid accusamus obcaecati! Repellat mollitia modi expedita laborum, at, illo cumque beatae molestiae inventore harum rerum dignissimos!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium aliquam quibusdam tempora eum aliquid accusamus obcaecati! Repellat mollitia modi expedita laborum, at, illo cumque beatae molestiae inventore harum rerum dignissimos!</p>
     </Container>
 );
 
